212cd5f603ee467cba80784c5e0da614f5114539
Bryan E Perez <bryaneperez@protonmail.com>
2023-12-06 04:21:15 -0500

    Fixed issues with Hash router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 import {HashRouter as Router, Routes, Route, Link} from "react-router-dom";
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import Experience from './pages/Experience';
 
+class App extends Component {
+  render () {
+    return (
+      <Router basename = "/">
+        <div>
+          <ul>
+            <li><Link to= "/">Home</Link></li>
+            <li><Link to = "/projects">Projects</Link></li>
+            <li><Link to = "/experience">Experience</Link></li>
+          </ul>
 
-function App() {
-  return (
-    <div className="App">
-      <Router basename='/'>
-        <Routes>
-          <Route path ="/" element = {<Home/>} />
-          <Route path ="/projects" element = {<Projects/>} />
-          <Route path ="/experience" element = {<Experience/>} />
-        </Routes>
+          <hr />
+          <Routes>
+
+          <Route exact path ="/" component={Home} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/experience" component={Experience} />
+          </Routes>
+        </div>
       </Router>
-    </div>
-  );
+    );
+  }
 }
 
+const Home = () => <div><h2>Home</h2></div>
+const Projects = () => <div><h2>Projects</h2></div>
+const Experience = () => <div><h2>Experience</h2></div>
 export default App;
